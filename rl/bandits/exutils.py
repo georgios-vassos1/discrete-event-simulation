95347375f2dc543811431229ed51b17000dc471d
georgios.vassos1 <georgios.vassos1@maersk.com>
2023-01-23 19:50:27 +0100

    Small fix

--- rl/bandits/exutils.py
+++ rl/bandits/exutils.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def read_pickle(task_id: int, from_path: str="/Users/gva/pcmabinf/OpenML-CC18/"):
+def read_pickle(task_id: int, from_path: str="~/pcmabinf/OpenML-CC18/"):
     return pd.read_pickle(from_path + f"{task_id}")
 
 
