0babd298c6fb3c0d6e1240c7bdff6248c473b6f1
georgios.vassos1 <georgios.vassos1@maersk.com>
2023-02-24 10:16:57 +0100

    CLeaning

--- rl/misc/inventory/mcts.R
+++ rl/misc/inventory/mcts.R
@@ -1,144 +1,152 @@
 rm(list=ls())
-setwd("~/solutions/rl/misc/inventory")
+source("~/solutions/rl/misc/inventory/mdp.R")
 
-Inventory <- function(...) {
-  # H : length of horizon
-  # M : inventory capacity
-  # x0: initial state
-  # rDemand: random demand sampling
-  # h : holding cost
-  # p : shortage cost
-  # K : set-up cost
-  args  <- list(...)
-  inv   <- new.env(hash = FALSE, parent = emptyenv())
-  inv$H       <- ifelse(!is.null(args[["H"]]) ,      args[["H"]],       3L   )
-  inv$M       <- ifelse(!is.null(args[["M"]]) ,      args[["M"]],       20L  )
-  inv$x0      <- ifelse(!is.null(args[["x0"]]),      args[["x0"]],       5L  )
-  inv$rDemand <- ifelse(!is.null(args[["rDemand"]]), args[["rDemand"]], function(n=1L) sample(seq(0L,9L), n, TRUE))
-  inv$h       <- ifelse(!is.null(args[["h"]]) ,      args[["h"]],       function(x) 1.0*x)
-  inv$p       <- ifelse(!is.null(args[["p"]]) ,      args[["p"]],       function(x) 1.0*x)
-  inv$K       <- ifelse(!is.null(args[["K"]]) ,      args[["K"]],       5.0  )
-  inv$X  <- NULL # State space
-  inv$A  <- NULL # Action space
-  inv$D  <- NULL # Demand space
-  inv$EC <- NULL # Expected cost
-  inv$V  <- NULL # Value function
-  class(inv) <- "Inventory"
-  return(inv)
+## Monte Carlo Tree Search
+Node <- function(env, state, action=0L, reward=NULL, parent=NULL, ...) {
+  node <- new.env(hash = FALSE, parent = emptyenv())
+  node$mdp    <- env
+  node$parent <- parent
+  node$state  <- state
+  node$action <- action
+  node$reward <- reward
+  node$children <- list()
+  class(node)   <- "Monte Carlo Tree Node"
+  return(node)
 }
 
-init.inv <- function(env, X, A, D, ...) {
-  # Store the state space X, action space A, and demand space D
-  env$X <- X
-  env$A <- A
-  env$D <- D
+MCTS <- function(Cp=1.0, ...) {
+  MCTS          <- new.env(hash = FALSE, parent = emptyenv())
+  MCTS$Qx       <- list()
+  MCTS$Nx       <- list()
+  MCTS$Px       <- list()
+  MCTS$N        <- list()
+  MCTS$Cp       <- Cp
+  class(MCTS)   <- "Monte Carlo Tree Search"
+  return(MCTS)
 }
 
-expected_cost <- function(env, ...) {
-  # Computes the expected cost for (X,A,D) with lost demand, capacity limit M, and fixed setup cost K
-  env$EC <- matrix(NA, nrow = env$M+1L, ncol = env$M+1L)
-  for (x in env$X) {
-    z <- outer(x+env$A, env$D, '-') # -t(z) = outer(env$D, x+env$A, '-')
-    env$EC[x+1L,] <- rowSums((1/length(env$D))*(
-      env$h(pmin(pmax(z, 0.0), env$M)) + t(env$p(pmax(-t(z), 0.0))))) + env$K*(env$A > 0.0)
+is_fully_expanded <- function(node) {
+  actions <- get_actions(node$mdp, node$state)
+  if (length(actions) == length(node$children)) {
+    return(TRUE)
+  }
+  FALSE
+}
+
+uct <- function(mcts, x) {
+  idx <- as.character(x)
+  action <- which.min(unlist(mcts$Qx[[idx]]) + 2.*mcts$Cp*sqrt(2.*log(mcts$N[[idx]]) / unlist(mcts$Nx[[idx]])))
+  as.numeric(names(action))
+}
+
+get_child <- function(node, a) {
+  ida    <- as.character(a)
+  r      <- reward(node$mdp, node$state, a)
+  next_x <- rNextState(node$mdp, node$state, a)
+  children <- unname(sapply(node$children, function(x) x$state))
+  cdx <- which(children == next_x)
+  if (length(cdx) == 1L) {
+    return(node$children[[cdx]])
+  }
+  node$children[[ida]] <- Node(node$mdp, state = next_x, action = a, reward = r, parent = node)
+  return(node$children[[ida]])
+}
+
+expand <- function(node, mcts, ...) {
+  if (is_fully_expanded(node)) {
+    return(node)
+  }
+  idx     <- as.character(node$state)
+  actions <- setdiff(as.character(get_actions(node$mdp, node$state)), names(node$children))
+  ida     <- sample(actions, 1L)
+  action  <- as.numeric(ida)
+  node$children[[ida]] <- get_child(node, action)
+  if (is.null(mcts$N[[idx]])) {
+    mcts$N[[idx]]  <- 0L
+  }
+  if (is.null(mcts$Nx[[idx]][[ida]])) {
+    mcts$Nx[[idx]][[ida]] <- 0L
+    mcts$Qx[[idx]][[ida]] <- 0.0
+  }
+  return(get_child(node, action))
+}
+
+select <- function(node, mcts) {
+  if (!is_fully_expanded(node)) {
+    return(node)
+  } else {
+    actions <- names(node$children)
+    action  <- uct(mcts, node$state)
+    return(get_child(node, action))
   }
 }
 
-value_function <- function(env, gamma=1.0, ...) {
-  # Value function computation for D~DU(0,9)
-  if (is.null(env$EC)) {
-    env$EC <- expected_cost(env)
+back_propagate <- function(node, mcts, r, child) {
+  idx <- as.character(node$state)
+  ida <- as.character(child$action)
+  mcts$N[[idx]] <- mcts$N[[idx]] + 1L
+  mcts$Nx[[idx]][[ida]] <- mcts$Nx[[idx]][[ida]] + 1L
+  mcts$Qx[[idx]][[ida]] <- (mcts$Nx[[idx]][[ida]] * mcts$Qx[[idx]][[ida]] + r) / (mcts$Nx[[idx]][[ida]] + 1.0)
+  if (!is.null(node$parent)) {
+    back_propagate(node$parent, mcts, node$reward+r, node)
   }
-  env$V <- matrix(NA, nrow = env$H+1L, ncol = env$M+1L)
-  env$V[env$H+1L,] <- 0.0
-  dim.D <- length(env$D)
-  for (i in seq(env$H,1L)) {
-    for (x in env$X) {
-      next_x <- pmin(pmax(outer(x+env$A, env$D, '-'), 0.0), env$M) + 1L
-      EV     <- c(matrix(env$V[i+1L,next_x], ncol=dim.D) %*% rep(1./dim.D,dim.D))
-      env$V[i,x+1L] <- min(env$EC[x+1L,] + gamma*EV)
+}
+
+simulate <- function(node, mcts, gamma=0.99, ...) {
+  x   <- node$state
+  idx <- as.character(x)
+  # Initialization
+  total_reward <- 0.0
+  depth  <- 0L
+  visits <- NULL
+  visits[[idx]] <- 1L
+  # Simulation
+  while (rbinom(1L, 1L, 1./visits[[idx]])) {
+    # Choose an action to execute
+    action <- sample(get_actions(node$mdp, x), 1L)
+    # Execute the action
+    r <- reward(node$mdp, x, action) # Observe reward
+    next_x <- rNextState(node$mdp, x, action) # Transit to next state
+    # Discount the reward
+    total_reward <- total_reward + (gamma)^(depth) * r
+    depth <- depth + 1L
+    # Update number of visits
+    idx   <- as.character(next_x)
+    if (is.null(visits[[idx]])) {
+      visits[[idx]] <- 1L
+    } else {
+      visits[[idx]] <- visits[[idx]] + 1L
     }
   }
+  total_reward
 }
 
-env <- Inventory()
-init.inv(env, seq(0L,env$M), seq(0L,env$M), seq(0L,9L))
-expected_cost(env)
-value_function(env)
-env$V
-
-# AMS object
-AMS <- function(...) {
-  AMS      <- new.env(hash = FALSE, parent = emptyenv())
-  args     <- list(...)
-  AMS$H    <- args[["H"]]
-  AMS$X    <- args[["X"]]
-  AMS$A    <- args[["A"]]
-  AMS$cX   <- length(args[["X"]]) # Cardinality of state space
-  AMS$cA   <- length(args[["A"]]) # Cardinality of action space
-  AMS$V    <- NULL
-  class(AMS) <- "Adaptive multi-stage sampler"
-  return(AMS)
+rollout <- function(mcts, root, ...) {
+  node <- select(root, mcts)
+  child <- expand(node, mcts)
+  r     <- simulate(child, mcts)
+  back_propagate(node, mcts, r, child)
 }
 
-init.ams <- function(ams, ...) {
-  ams$V   <- matrix(NA,  nrow = (ams$H+1L), ncol = length(ams$X))
+root <- Node(env, 5L)
+mcts <- MCTS()
+for (i in seq(500L)) {
+  rollout(mcts, root)
 }
+min(unlist(mcts$Qx[["5"]]))
 
-reward <- function(x, a, D=seq(0L,9L), h=1.0, p=1.0, K=5.0, M=20L, ...) {
-  (1.0/length(D)) * sum(h*pmin(pmax(x+a-D,0.0),M) + p*pmax(D-x-a, 0.0)) + K*(a>0)
-}
-
-rNextState <- function(x, a, D=seq(0L,9L), M=20L, ...) sample(pmin(pmax(x+a-D, 0L), M), 1L)
-
-ams_search <- function(ams, x, i, Ni, gamma=1.0, ...) {
-  stopifnot(Ni>=ams$cA)
-  # Terminal condition
-  if (i==ams$H) {
-    ams$V[i+1L,x+1L] <- 0.0
-    return(0.0)
-  }
-  # Initialization (fix this to run only once)
-  rx <- rep(NA, ams$cA)
-  EV <- rep(NA, ams$cA)
-  Nx <- rep(NA, ams$cA)
-  for (j in seq_along(ams$A)) {
-    # Compute reward
-    rx[j] <- reward(x, ams$A[j])
-    # Sample next state
-    y      <- rNextState(x, ams$A[j])
-    # Compute expected future value
-    EV[j]  <- ams_search(ams, y, i+1L, Ni, gamma)
-    # Update counter
-    Nx[j]  <- 1L
-  }
-  # Main loop
-  n <- length(ams$A)
-  while (n <= Ni) {
-    Qx <- rx + gamma * (EV/Nx) - sqrt(2*log(n)/Nx)
-    # Optimal action
-    j_star <- which.min(Qx)
-    a_star <- ams$A[j_star]
-    # Sample next state
-    y      <- rNextState(x, a_star)
-    # Compute  the future value of the state y
-    EV[j_star] <- EV[j_star] + ams_search(ams, y, i+1L, Ni, gamma)
-    # Update data containers
-    Nx[j_star] <- Nx[j_star] + 1L
-    n <- n + 1L
-  }
-  v <- sum((Nx/Ni)*(rx+gamma*EV/Nx))
-  # v <- min(rx+gamma*EV/Nx)
-  ams$V[i+1L,x+1L] <- v
-  return(v)
-}
-
-ams <- AMS(H=3L, X=seq(0L,20L), A=seq(0L,20L))
-init.ams(ams)
-ams_search(ams, x = 0L, i = 0L, Ni = 35L, gamma = 0.7)
 
 ## OBSOLETE
 if (FALSE) {
+  # reward <- function(x, a, D=seq(0L,9L), h=1.0, p=1.0, K=5.0, M=20L, ...) {
+  #   (1.0/length(D)) * sum(h*pmin(pmax(x+a-D,0.0),M) + p*pmax(D-x-a, 0.0)) + K*(a>0)
+  # }
+  
+  # rNextState <- function(x, a, D=seq(0L,9L), M=20L, ...) sample(pmin(pmax(x+a-D, 0L), M), 1L)
+  
+  # get_actions <- function(x, M=20L, ...) {
+  #   unique(pmax(seq(0L,M)-x,0L))
+  # }
   # Slow implementation for testing
   ECb <- matrix(NA, nrow = env$M+1L, ncol = env$M+1L)
   for (x in seq(0L,env$M)) {
